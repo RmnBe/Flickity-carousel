8e4514e9336df0b22f621d2c389682143c84387c
Roman Bednarz <roman.bednarz@gmail.com>
2019-04-21 00:31:09 +0200

    done with troubles

--- js/script.js
+++ js/script.js
@@ -2,7 +2,20 @@ var carousel = document.querySelector('.main-carousel');
 var flkty = new Flickity( carousel, {
 	imagesLoaded: true,
 	percentPosition: false,
-	pageDots: true,
+	pageDots: false,
 	cellAlign:'left',
 	contain: true,
+	hash: true,
+	freeScroll: true,
+	wrapAround: true,
+});
+var restartBtn = document.querySelector('.myButton');
+restartBtn.addEventListener('click', function(){
+	flkty.next(true);
+	flkty.select( 0 );
+})
+var progressBar = document.querySelector('.progress-bar')
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
